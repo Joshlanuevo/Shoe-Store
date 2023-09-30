066ce7e4c3ef4673d2938d7372bd2be11c37e9e6
Joshlanuevo <lanuevo68@gmail.com>
2023-09-30 15:23:41 +0800

    banner-head-added

---
+++ src/app/components/AnnouncementBar.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { useState, useEffect } from 'react';
+import 'animate.css'; // Import animate.css
+
+const AnnouncementBar = () => {
+    const announcements: string[] = [
+        "30 days return 💰",
+        "Free delivery around manila 🚚"
+    ];
+
+    const [announcementIndex, setAnnouncementIndex] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAnnouncementIndex((prevIndex) => (prevIndex + 1) % announcements.length);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [announcementIndex, announcements.length]);
+
+    if (!announcements || announcements.length === 0) {
+        return null;
+    }
+
+    const capitalizeWords = (sentence: string) => {
+        return sentence.split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }).join(' ');
+    };
+
+    const capitalizedAnnouncement = capitalizeWords(announcements[announcementIndex]);
+
+    return (
+        <div key={announcementIndex} className="bg-[#2a2727] font-semibold p-1 text-center text-white">
+            <p className="text-sm animate__animated animate__fadeIn">
+                {capitalizedAnnouncement}
+            </p>
+        </div>
+    );
+};
+
+export default AnnouncementBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Header from './components/Header'
+import AnnouncementBar from './components/AnnouncementBar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AnnouncementBar />
+        <Header />
+        <main className="container">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
