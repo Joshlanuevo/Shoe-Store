ab26c682a0a43b2e38d9b2188d4c3dd4107ed73d
Joshlanuevo <lanuevo68@gmail.com>
2023-09-30 10:47:39 +0800

    hover-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Header from "./components/Header"
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <h1>Keep Grinding!</h1>
-    </div>
+    </>
   )
 }
