55386effdf13eff282ca15eadf75209ac7d6dbc3
Joshlanuevo <lanuevo68@gmail.com>
2023-10-01 16:23:47 +0800

    main-image-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+// app/page.tsx
+import Image from "next/image";
+import AirJordan from "../../public/images/main-page/jordan-1.jpg";
 
 export default function Home() {
   return (
-    <>
-      <h1>Keep Grinding!</h1>
-    </>
-  )
+    <div className="h-screen w-screen relative">
+      <div className="h-full w-full relative">
+        <Image 
+          src={AirJordan} 
+          layout="fill" 
+          objectFit="cover"
+          alt="Jordan One Shoes" 
+          quality={100}
+          className="main-image"
+        />
+        <div className="overlay"></div>
+      </div>
+    </div>
+  );
 }
