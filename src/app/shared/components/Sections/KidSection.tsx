e994e8a514db9cebef4402b1c62a70ea8e60261d
Joshlanuevo <lanuevo68@gmail.com>
2023-10-06 14:18:15 +0800

    Designing Kids Section

--- src/app/shared/components/Sections/KidSection.tsx
+++ src/app/shared/components/Sections/KidSection.tsx
@@ -1,55 +1,58 @@
 import Image from "next/image";
 import Button from "../FormElements/Button";
 
-import AirJordanImage from "../../../../../public/images/main-page/air-jordan.jpg";
+import KidsImage from "../../../../../public/images/main-page/kids-section.png";
+import KidsImageTwo from "../../../../../public/images/main-page/kids-section-2.png";
 
 const KidSection = () => {
-    return ( 
-        <div className="z-0 h-screen w-screen relative flex items-center justify-center flex-col">
-            <div className="h-full w-full relative">
-                <Image
-                    src={AirJordanImage}
+    return (
+        <div className="z-0 h-screen w-screen relative flex">
+            <div className="kids-section-images flex-1 flex flex-wrap sm:flex-col md:flex-row lg:flex-row">
+                <div className="relative w-full sm:w-1/2 md:w-1/2 lg:w-1/2">
+                    <Image
+                    src={KidsImageTwo}
                     layout="fill"
                     objectFit="cover"
+                    objectPosition="center"
                     alt="Jordan One Shoes"
                     quality={100}
-                />
-                <div className="absolute text-center text-white top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <p className="font-thin text-base sm:text-lg lg:text-xl xl:text-2xl">
-                    Experience the epitome of sneaker culture with our exclusive
-                    collection of Nike and Jordan 1 shoes. From classic silhouettes to
-                    limited releases, we offer the ultimate selection for sneaker
-                    enthusiasts and fashion-forward individuals alike.
+                    />
+                </div>
+                <div className="relative w-full sm:w-1/2 md:w-1/2 lg:w-1/2">
+                    <Image
+                    src={KidsImage}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    alt="Jordan One Shoes"
+                    quality={100}
+                    />
+                </div>
+            </div>
+
+            <div className="absolute top-0 right-0 h-full flex flex-col items-end justify-end p-8">
+                <div className="text-right">
+                    <h1 className="font-extrabold text-6xl text-white">Kids Section</h1>
+                    <p className="sm:text-lg lg:text-md xl:text-md text-black">
+                        Easy On and Off Shoes
                     </p>
-                    <div className="flex flex-row sm:flex-row items-center justify-center mt-10">
-                    {/* Button for Men's */}
-                    <Button
-                        href="/"
-                        className="bg-white text-black hover:bg-[#fdb49c] hover:text-white font-bold py-4 px-6 mx-2 mb-2 sm:mb-0 sm:mx-4 rounded whitespace-nowrap text-sm sm:text-base lg:text-lg"
-                        onClick={() => {
-                        // Handle click logic
-                        }}
-                        type="button"
-                        disabled={false}
-                    >
-                        Shop Men's
-                    </Button>
-                    <Button
-                        href="/"
-                        className="bg-transparent text-white hover:bg-[#fdb49c] hover:text-white border-2 border-white font-bold py-4 px-6 mx-2 mb-2 sm:mb-0 sm:mx-4 rounded whitespace-nowrap text-sm sm:text-base lg:text-lg"
-                        onClick={() => {
-                        // Handle click logic
-                        }}
-                        type="button"
-                        disabled={false}
-                    >
-                        Shop Women's
-                    </Button>
+                    <div className="flex flex-col sm:flex-row items-end justify-end mt-4 mb-10">
+                        <Button
+                            href="/"
+                            className="bg-white text-black hover:bg-[#fdb49c] hover:text-white font-bold py-4 px-8 mx-2 mb-2 sm:mb-0 sm:mx-4 rounded whitespace-nowrap text-sm sm:text-base lg:text-lg"
+                            onClick={() => {
+                                // Handle click logic
+                            }}
+                            type="button"
+                            disabled={false}
+                        >
+                            Shop Kids
+                        </Button>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
- 
+};
+
 export default KidSection;
