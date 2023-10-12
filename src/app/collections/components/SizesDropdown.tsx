9f69c20ec689473a3a45a732ed73d20b5406cb40
Joshlanuevo <lanuevo68@gmail.com>
2023-10-12 15:56:46 +0800

    Adding style on sizes menu

--- src/app/collections/components/SizesDropdown.tsx
+++ src/app/collections/components/SizesDropdown.tsx
@@ -1,9 +1,24 @@
+import React from 'react';
+
+const sizes = [
+  5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13
+];
+
 const SizesDropdown = () => {
-    return ( 
-        <div>
-            Sizes
-        </div>
-    );
+  return (
+    <ul className="menu bg-base-200 w-56 rounded-box">
+      <li>
+        <details open>
+          <summary>Sizes</summary>
+          <ul>
+            {sizes.map((size) => (
+              <li key={size}><a>{size}</a></li>
+            ))}
+          </ul>
+        </details>
+      </li>
+    </ul>
+  );
 }
- 
+
 export default SizesDropdown;
